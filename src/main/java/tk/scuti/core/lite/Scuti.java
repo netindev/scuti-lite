062257a65be4b2932e76d60670039f8348904a07
netindev <netinmo@hotmail.com>
2018-11-02 09:58:14 -0300

    organize imports

--- src/main/java/tk/scuti/core/lite/Scuti.java
+++ src/main/java/tk/scuti/core/lite/Scuti.java
@@ -6,8 +6,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
